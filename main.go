ff62872662257324ae7a22eb78b8074fa96d9ba1
iamtonmoy0 <tonmoy2code@gmail.com>
2023-02-27 16:33:41 -0500

    bot added

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	err := config.ReadConfig()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	bot.start()
+	<-make(chan struct{})
+	return
+}
